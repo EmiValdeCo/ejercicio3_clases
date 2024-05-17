7053968128e3c5a2b30594df5d9142dcc69121ac
EmiValdeCo <20190010@ricaldone.edu.sv>
2024-05-17 09:06:11 -0600

    first commit

---
+++ src/data/creador.js
@@ -0,0 +1,9 @@
+const creadorData = [
+{
+        id: '1',
+        name: 'Emiliano Valdés',
+        desc: `Ejercicio 3, se modificó para mostrar colores y más datos`
+}
+];
+
+export default creadorData;
